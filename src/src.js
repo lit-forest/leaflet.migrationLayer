1a2822e3510677f2d8e154fc157d0dd20d6fcf5f
Find the undefined <1102977704@qq.com>
2020-09-25 15:12:45 +0800

    Update src.js

--- src/src.js
+++ src/src.js
@@ -577,20 +577,26 @@
             // this._map.on('movestart ', function () {
             //     that.migration.pause();
             // });
-            if(!this._map.listens("moveend"))
+            if(!this._map.listens("moveend")){
               this._map.on('moveend', function () {
                 that.migration.play();
                 that._draw();
               });
-            if(!this._map.listens('zoomstart'))
+            }
+              
+            if(!this._map.listens('zoomstart')){
               this._map.on('zoomstart ', function () { that.container.style.display = 'none' });
-            if(!this._map.listens('zoomend'))
+            }
+              
+            if(!this._map.listens('zoomend')){
               this._map.on('zoomend', function () {
                 if (that._show) {
                   that.container.style.display = ''
                   that._draw();
                 }
               });
+            }
+             
         },
         _draw: function () {
             var bounds = this._map.getBounds();
@@ -643,7 +649,7 @@
             this.container.parentNode.removeChild(this.container);
             //移除事件监听
             //this._map.clearAllEventListeners();
-            //this.mapHandles = [];
+            this.mapHandles = [];
         }
 
     });
